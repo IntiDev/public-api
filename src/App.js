520009134a36e90f0d59e21e3dc74529fbc4c789
Inti Dev <yeseniase@outlook.it>
2020-09-12 15:08:44 -0500

    [ADD] test api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const API_URL = 'https://ghibliapi.herokuapp.com/films';
+
+  const fetchAPI = (url) => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      //setArray(data.data);
+      console.log(data);
+    })
+    .catch( error => console.error(error));
+  };
+
+    fetchAPI(API_URL)
+
   return (
     <div className="App">
       <header className="App-header">
-        Public Api
+        <img src={logo}/>
       </header>
     </div>
   );
